6aec35ffff1e26423f0139c0c1a4ea74899fdcf2
Rory-mbp <roconnor@Rorys-MBP.fritz.box>
2017-07-29 19:13:23 +1200

    Node api server reads api key from file.

---
+++ pubg-compare-api/server.js
@@ -0,0 +1,63 @@
+// server.js
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cors = require('cors')
+var fs = require('fs');
+
+var fileApiKey = fs.readFileSync('KEYDATA', 'utf8');
+var port = process.env.PORT || 3001;
+console.log(fileApiKey);
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors())
+
+var router = express.Router();
+
+//pubg api constant
+const {PubgAPI, PubgAPIErrors} = require('pubg-api-redis');
+
+// pubg api redis cache config
+const api = new PubgAPI({
+    apikey: fileApiKey.toString(),
+    redisConfig: {
+        host: '127.0.0.1',
+        port: 6379,
+    },
+});
+
+// middleware to use for all requests
+//router.use(function(req, res, next) {
+//    // do logging
+//    console.log('Something is happening.');
+//    next(); // make sure we go to the next routes and don't stop here
+//});
+
+
+router.route('/playername/:player_name')
+
+    // get the player with that pubg name
+    .get(function (req, res) {
+        api.profile.byNickname(req.params.player_name)
+            .then((data) => { 
+                res.json(data);
+            });
+    });
+
+router.route('/steamid/:steam_id')
+
+    // get the player with that steam id
+    .get(function (req, res) {
+        api.profile.bySteamId(req.params.steam_id)
+            .then((data) => {
+                res.json(data);
+            });
+    });
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Listening on ' + port);
